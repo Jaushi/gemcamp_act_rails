429b4ffa4f65f5bf33d87f6622d903e95c48a022
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-10-03 21:25:21 +0800

    Fix and modified the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root 'products#index'
-  resources :products 
+  root 'products#index' # Adjust this to your actual root controller and action
+
+  get 'about_us', to: 'products#about_us', as: 'about_us'
+  get 'contact', to: 'products#contact', as: 'contact'
+  resources :products, only: [:index, :new, :create, :show,:edit, :update, :destroy]
+  resources :products do
+    collection do
+      get 'search'
+    end
+  end
+
 end
