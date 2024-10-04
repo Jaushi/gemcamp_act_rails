31f91d842862cf4db3ba4e53e826a3d662cb7f3b
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-10-04 22:32:45 +0800

    Modify: Validations to warn user to complete input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
     def create
         @product = Product.new(product_params)
         if @product.save
-            redirect_to @product
+            redirect_to @product, notice: 'Product was successfully created.'
         else
             render :new
         end
@@ -36,12 +36,12 @@ class ProductsController < ApplicationController
     def destroy
         @product = Product.find(params[:id])
         @product.destroy
-        redirect_to products_path
+        redirect_to products_path, notice: 'Product was successfully deleted.'
     end
-
+    
     private
 
     def product_params
-        params.require(:product).permit(:name, :description, :price, :image)
+    params.require(:product).permit(:name, :content, :quantity, :price, :available, :released_at, :discount)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
-belongs_to :category
-has_many :order_items
-    validates :name, presence: true, length: { maximum: 255 }
-    validates :content, presence: true
-    validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    validates :available, inclusion: { in: [true, false] }
-    validates :released_at, presence: true
-    validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+    belongs_to :category, optional: true
+    has_many :order_items
+
+    validates :name, presence: { message: "- Product name can't be blank" }, length: { maximum: 255, message: "- Product name is too long (maximum is 255 characters)" }
+    validates :content, presence: { message: "- Product description can't be blank" }
+    validates :quantity, presence: { message: "- Quantity can't be blank" }, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "- Quantity must be a non-negative integer" }
+    validates :price, presence: { message: "- Price can't be blank" }, numericality: { greater_than_or_equal_to: 0, message: "- Price must be a non-negative number" }
+    validates :available, inclusion: { in: [true, false], message: "- Available must be true or false" }
+    validates :released_at, presence: { message: "- Release date can't be blank" }
+    validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: "- Discount must be between 0 and 100" }
 end
