ad88aed67fa3dd5f0f71616c933b44ec21a42dd1
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-10-04 22:26:41 +0800

    Fix: Routing and index page for better user interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'products#index' # Adjust this to your actual root controller and action
+  root 'products#index' 
 
   get 'about_us', to: 'products#about_us', as: 'about_us'
   get 'contact', to: 'products#contact', as: 'contact'
-  resources :products, only: [:index, :new, :create, :show,:edit, :update, :destroy]
+  
   resources :products do
     collection do
       get 'search'
